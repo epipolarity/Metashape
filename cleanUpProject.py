ae8b889ca6e4a9b124c4f3c8d87e4bb2d55728e5
James White <jwhite@gbg.co.uk>
2023-05-15 18:35:41 +0100

    Added clean up project script

---
+++ cleanUpProject.py
@@ -0,0 +1,24 @@
+import Metashape
+
+def clean_up():
+
+    depth_maps = 0
+    depth_maps_chunks = 0
+    orthophotos_chunks = 0
+
+    app = Metashape.app
+    for chunk in app.document.chunks:
+        if chunk.depth_maps:
+            depth_maps += len(chunk.depth_maps.items())
+            chunk.depth_maps = None
+            depth_maps_chunks += 1
+        for ortho in chunk.orthomosaics:
+            ortho.removeOrthophotos()
+            orthophotos_chunks += 1
+
+    if depth_maps + depth_maps_chunks + orthophotos_chunks > 0:
+        message = f"{depth_maps} Depth Maps removed from {depth_maps_chunks} chunk(s)\nOrthophotos removed from 'up to' {orthophotos_chunks} chunk(s)\n\nSave Project now?"
+        if app.getBool(label=message):
+            app.document.save()
+            
+Metashape.app.addMenuItem("Custom Functions/Clean Up Project for Archive", clean_up)
